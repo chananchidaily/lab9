70641aa315b5afe7ecc3de7837c80de5cb0545f6
tonkhao <chananchida_tha@cmu.ac.th>
2022-12-23 16:29:24 +0700

    okkk

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -32,8 +32,10 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin, name[i]) ;
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
